a14b28da2efc5e6189d54606ce5921d5bcd24f3d
Juanes Velasquez <juesvema_12@hotmail.com>
2018-06-26 08:39:20 -0500

    Styles added to the header

--- app/sections/Header.js
+++ app/sections/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export class Header extends Component {
     constructor(props) {
@@ -17,9 +17,28 @@ export class Header extends Component {
     render() {
         let display = this.state.isLoggedIn ? 'Sample User' : this.props.message;
         return (
-            <Text onPress={this.toogleUser}>
-                {display}
-            </Text>
+            <View style={styles.headStyle}>
+                <Text 
+                style={styles.headText}
+                onPress={this.toogleUser}>
+                    {display}
+                </Text>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    headText: {
+        textAlign: 'right',
+        color: '#fff',
+        fontSize: 20
+    },
+    headStyle: {
+        paddingTop: 30,
+        paddingBottom: 10,
+        paddingRight: 10,
+        backgroundColor: '#35605a',
+        flex: 1
+    }
+})
--- app/views/Home.js
+++ app/views/Home.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Header } from '../sections/Header'
 
 class Home extends Component {
     render() {
         return (
-            <View>
-              <Text>Home</Text>
-              <Header message = 'Press to Login' />
+            <View style={styles.container}>
+                <Header message='Press to Login' />
+                <Text style={{flex:8}}>Home</Text>
+                <Text style={{flex:6}}>Example</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1
+  }
+});
+
 
 export default Home;
