c13c46f0c4f9f807f4c6c1df4a0d08e4b9d6f8ab
Juanes Velasquez <juesvema_12@hotmail.com>
2018-06-27 10:14:18 -0500

    Questions component and QuizQuestions file added

---
+++ app/data/QuizQuestions.js
@@ -0,0 +1,67 @@
+export const QuizData = {
+    questions: [
+        {
+            question: 'Which of the following is NOT a reserved word?',
+            answer1: 'and',
+            answer2: 'debugger',
+            answer3: 'instanceOf',
+            answer4: 'while',
+            correctAnswer: 'answer1',
+            key: '1A'
+        },
+        {
+            question: 'What does Number(null) return?',
+            answer1: 'undefined',
+            answer2: 'null',
+            answer3: '0',
+            answer4: '1',
+            correctAnswer: 'answer3',
+            key: '1B'
+        },
+        {
+            question: 'Which of the following is a standard RegExp literal format?',
+            answer1: 'var re = /pattern/flags',
+            answer2: 'var re = "pattern" % "flags"',
+            answer3: 'var re = RegExp(pattern, flags)',
+            answer4: 'var re = RegExp/pattern/flags',
+            correctAnswer: 'answer1',
+            key: '1C'
+        },
+        {
+            question: 'What happens in the following code? \nvar x = { name: "hello" };\n console.log(y);',
+            answer1: 'null is written to the console.',
+            answer2: 'undefined is written to the console.',
+            answer3: 'An empty string is written to the console.',
+            answer4: 'Reference error',
+            correctAnswer: 'answer4',
+            key: '1D'
+        },
+        {
+            question: 'What does JSONP stand for?',
+            answer1: 'JavaScript Object Notation Predicate',
+            answer2: 'JavaScript Object Notation Protocol',
+            answer3: 'JavaScript Object Notation Parser',
+            answer4: 'JavaScript Object Notation with Padding',
+            correctAnswer: 'answer4',
+            key: '1E'
+        },
+        {
+            question: 'What will console.log( typeof [1,2] ) print out?',
+            answer1: 'string',
+            answer2: 'object',
+            answer3: 'number',
+            answer4: 'array',
+            correctAnswer: 'answer2',
+            key: '1F'
+        },
+        {
+            question: 'Local scope can be defined within which of the following?',
+            answer1: 'function and while',
+            answer2: 'function only',
+            answer3: 'Any JavaScript block',
+            answer4: 'function and if',
+            correctAnswer: 'answer2',
+            key: '1G'
+        }
+    ]
+}
